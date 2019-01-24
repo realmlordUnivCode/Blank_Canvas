47344223e088b8eb5ac81d7f01bd7f36e42ae921
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-01-24 14:07:30 -0500

    Created a solid template for the item screen,
    Items screen is fully customizable, any items can be coded into the template.

---
+++ Items.js
@@ -0,0 +1,19 @@
+<head>
+<script src = gameStart.js>
+</head>
+
+<script>
+#This page is the items page, you can customize it and add more entries to the items by copying the below template and pasting it multiple times
+</script>
+
+
+<Button name=(buttonName),onClick=(Change this to whatever you want the function to be named)> (insert Item Name here.)<charset = UTF 8>
+
+<script>
+function.(functionName){
+#(insert items code here, minus the pound symbols)
+#example item:
+# var use = alert("You have used a bag of gold, enjoy your money!")
+# (currency Variable Name)==(Currency Variable Name) + 2000
+
+</script>
